8a3b550d0c0199e0a271cb7f9835720474c59fe3
ivanivan <ivanivan@cisco.com>
2026-06-04 08:23:45 +0200

    fix(footer): wrap footer text in a link to the GitHub repository

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -87,7 +87,7 @@
 	</main>
 
 	<footer class="border-t border-gray-200 bg-white py-4 text-center text-sm text-gray-500">
-		Studia &mdash; Reverse Learning App
+		<a href="https://github.com/Manvan33/studia">Studia - Reverse Learning App</a>
 	</footer>
 </div>
 
